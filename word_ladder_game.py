3a327a038333edfa48bcf1a2d5a8a41bd398264c
Mei <msmeiyong@gmail.com>
2019-11-24 11:58:33 +0000

    Update word_ladder_game.py

--- word_ladder_game.py
+++ word_ladder_game.py
@@ -5,7 +5,7 @@ Author: Mei Yong
 https://github.com/mei-yong/neo4j_python_word_ladder
 """
 
-# Initialise the graph db
+# Initialise the local graph db
 from py2neo import Graph 
 uri = "bolt://localhost:7687"
 user = "neo4j"
@@ -16,49 +16,20 @@ graph = Graph(uri=uri, user=user, password=password)
 # Function that takes the list of words and finds the ones that are related to each other by 1 character
 def identify_relationships(word_list):
     
-    
-    """ # broken code to fix
-    word_list = word3
-    
-    # Convert the list of word strings into a list of individual character lists
-    split_words = []
-    for word in word_list:
-        split_words.append(list(word))
-        
-    duplicate_split_words = [[word] * len(word) for word in split_words]
-    duplicate_split_words = [word for nested_list in duplicate_split_words for word in nested_list]
-    
-    #duplicate_split_words = [['a', 'a', 'a'],['a', 'a', 'a'],['a', 'a', 'a'],['a','b','a']]
-
-    x = 0
-    
-    for word in duplicate_split_words:
-        word[x] = '_'
-        if x == (len(word)-1):
-            x = 0
-        else:
-            x += 1
-        """
-        
-            
-        
-    
     # Create the list of buckets for identifying relationships
     bucket_list=[]
     for word in word_list:
-        if '_'+word[-2:] not in bucket_list:
-            bucket_list.append('_'+word[-2:])
-        if word[0]+'_'+word[2] not in bucket_list:
-            bucket_list.append(word[0]+'_'+word[2])
-        if word[:2]+'_' not in bucket_list:
-            bucket_list.append(word[:2]+'_')
+        for n in range(len(word)):
+            test_word = word[:n] + '_' + word[n+1:]
+            if test_word not in bucket_list:
+                bucket_list.append(test_word)
     
     # Add words to the buckets
     import re
     bucket_dict={}
     for bucket in bucket_list:
         regex = bucket.replace('_','[A-Za-z]')
-        bucket_dict[bucket] = [word for word in word3 if re.search(regex,word)]
+        bucket_dict[bucket] = [word for word in word_list if re.search(regex,word)]
     
     # Find the combinations between the words in the buckets
     from itertools import combinations
@@ -77,7 +48,15 @@ def create_nodes(word_list, node_label):
     tx = graph.begin() # Initialise transaction
     tx.run(query) # Run the Cypher query
     tx.commit() # Commit the Cypher query
-    
+
+# Function that creates/merges neo4j nodes in batches
+def create_nodes_in_batches(word_list, node_label, batch_size):
+    import math
+    start, end = 0, 500
+    for batch in range(math.ceil(len(word_list)/500)):
+        create_nodes(word_list[start:end], node_label=node_label)
+        start += 500
+        end += 500
 
 # Function that creates/merges neo4j relationships based on a dictionary of relationships
 def create_relationships(rel_dict, node_label):
@@ -90,42 +69,60 @@ def create_relationships(rel_dict, node_label):
                 tx.commit() # Commit the Cypher query
     
 
+# Function that takes a whole list of words and creates a neo4j gragh db based on it
+def convert_words_into_graphdb(list_of_all_words):
+    
+    min_len = len(min(list_of_all_words, key=len))
+    max_len = len(max(list_of_all_words, key=len))
+    
+    for word_len in range(min_len, max_len+1):
+        
+        # Find words of a particular length
+        word_list = [word for word in list_of_all_words if len(word)==word_len]
+        
+        # Find the relationships between the words of a particular length
+        rel_dict = identify_relationships(word_list)
+        
+        # Create nodes in batches of 500
+        create_nodes_in_batches(word_list=word_list, node_label='Word'+str(word_len), batch_size=500)
+        
+        # Create edges
+        create_relationships(rel_dict=rel_dict, node_label='Word'+str(word_len))
+        
+        
+#########################################################################################
+        
+        
+# Timing code execution
+import time
+import datetime
+start_time = time.time()
+
 
 # Import the text file list of dictionary words as a list
 # https://stackoverflow.com/questions/14676265/how-to-read-a-text-file-into-a-list-or-an-array-with-python
 with open("words.txt", 'r') as f:
     all_words = f.read().splitlines()
 
+# Get only words between 3-6 characters long
+all_words_max6 = [word for word in all_words if len(word) in range(3,7)]
 
-# Get only the 3-letter words
-word3 = [word for word in all_words if len(word)==3]
 
-# Get relationships for 3-letter words
-rel_dict = identify_relationships(word3)
+# Create nodes and edges based on the list of words
+convert_words_into_graphdb(list_of_all_words=all_words_max6)
 
-# Create nodes in batches of 500 or else it kills the local toaster aka laptop - because each transaction creates 500 nodes in one go
-import math
-start = 0 
-end = 500
-for batch in range(math.ceil(len(word3)/500)):
-    create_nodes(word3[start:end], node_label='Word3')
-    start += 500
-    end += 500
 
-# Create relationships - not in batches because each transaction only creates 1 relationship
-create_relationships(rel_dict, node_label='Word3')
+# Timing code execution
+execution_time = time.time() - start_time
+print(f"Code execution time: {str(datetime.timedelta(seconds=execution_time))}")
 
 
 
-# Do the same but for 4-letter words
-word4 = [word for word in all_words if len(word)==4]
-rel_dict4 = identify_relationships(word4)
-
-start = 0 
-end = 500
-for batch in range(math.ceil(len(word4)/500)):
-    create_nodes(word4[start:end], node_label='Word4')
-    start += 500
-    end += 500
-    
-create_relationships(rel_dict4, node_label='Word4')
+"""
+# Testing
+start_time = time.time()
+wordtest = [word for word in all_words if len(word) in range(3,5)]
+convert_words_into_graphdb(list_of_all_words=wordtest)
+execution_time = time.time() - start_time
+print(f"Code execution time: {str(datetime.timedelta(seconds=execution_time))}")
+"""
